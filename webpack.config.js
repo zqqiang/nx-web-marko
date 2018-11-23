8be4b70f848fb2a2255774b420551b0aec6fab23
zqqiang <qiangzhaoqing@gmail.com>
2018-11-23 07:56:44 -0800

    try webpack dev server with marko

--- webpack.config.js
+++ webpack.config.js
@@ -43,13 +43,29 @@ module.exports = {
   ],
   devServer: {
     contentBase: path.join(__dirname, "static"),
+    publicPath: "/static/",
     compress: true,
     stats: "errors-only",
     host: process.env.HOST,
     port: process.env.PORT || 8090,
-    open: true,
     after: function(app, server) {
-      app.get("/", require("./src/pages/home"));
+      require("app-module-path").addPath(__dirname);
+      require("marko/express");
+      require("marko/node-require");
+      require("marko/hot-reload").enable();
+      const templatesDir = "src/components/app";
+      require("fs").watch(templatesDir, function(event, filename) {
+        if (/\.marko$/.test(filename)) {
+          // Resolve the filename to a full template path:
+          var templatePath = path.join(templatesDir, filename);
+
+          console.log("Marko template modified: ", templatePath);
+
+          // Pass along the *full* template path to marko
+          require("marko/hot-reload").handleFileModified(templatePath);
+        }
+      });
+      app.get("/", require("src/pages/home"));
     }
   }
 };
